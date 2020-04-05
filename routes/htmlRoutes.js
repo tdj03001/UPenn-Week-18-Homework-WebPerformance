a6db7c430718f0b73c38b986954bf5f8551de3ad
tdj03001 <tyjenki@gmail.com>
2020-04-05 08:52:57 -0400

    updated filepath to '/'

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,9 +1,8 @@
 const router = require("express").Router();
 const path = require("path");
 
-
 router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
+  res.sendFile(path.join(__dirname, "/index.html"));
 })
 
 module.exports = router
